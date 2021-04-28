ad8f49f81cc80e34fc32792f2e7e43b26fd4765a
Alaa Sahloub <alaa.sahloub.eb@gmail.com>
2021-04-28 16:05:58 +0300

    handle check all todos

--- comps/CheckAll.js
+++ comps/CheckAll.js
@@ -1,12 +1,19 @@
+import { useContext } from "react";
 import { TodoContext } from "./ContextProvider";
 
 const CheckAll = () => {
     const { todosState, setTodosState } = useContext(TodoContext);
     
+    const handleCheck = () => {
+        setTodosState(
+            todosState.map((todo) => {
+                return  { ...todo, compleated: true }
+            })
+        )
+    }
+
     return (
-        <div>
-            
-        </div>
+        <input type="checkbox" onChange={handleCheck} />
     )
 }
 
